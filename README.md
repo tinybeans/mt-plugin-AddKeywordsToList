abacd37f0b581a224d2919ae9ee839fb1d07c2b2
tinybeans <register@tinybeans.net>
2011-06-21 15:50:32 +0900

    First commit.

---
+++ README.md
@@ -0,0 +1,8 @@
+# About
+
+Add keywords to the entry list screen.
+
+# Author
+
+Tomohiro Okuwaki
+http://www.tinybeans.net/blog/
---
+++ plugins/AddKeywordsToList/lib/AddKeywordsToList/L10N/ja.pm
@@ -0,0 +1,11 @@
+package AddKeywordsToList::L10N::ja;
+
+use strict;
+use base 'AddKeywordsToList::L10N';
+use vars qw( %Lexicon );
+
+our %Lexicon = (
+    'Add keywords to the entry list screen.' => 'ブログ記事一覧画面にキーワードを追加します。',
+);
+
+1;
